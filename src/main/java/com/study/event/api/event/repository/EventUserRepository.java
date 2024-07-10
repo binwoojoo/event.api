aad74b7ca7857dc1003937575a19cc629c55a026
oooowb <oooo940212@gmail.com>
2024-07-10 12:35:05 +0900

    이메일 인증코드 전송

--- src/main/java/com/study/event/api/event/repository/EventUserRepository.java
+++ src/main/java/com/study/event/api/event/repository/EventUserRepository.java
@@ -4,4 +4,8 @@ import com.study.event.api.event.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventUserRepository extends JpaRepository<EventUser, String> {
+
+    // query method로 jpql 생성
+    boolean existsByEmail(String email);
+
 }
