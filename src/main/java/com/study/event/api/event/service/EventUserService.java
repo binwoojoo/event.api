7bcd89e8bdf584b5d1a59eeea2cb4bb833d136da
oooowb <oooo940212@gmail.com>
2024-07-11 10:36:17 +0900

    인증 코드 체크 로직

--- src/main/java/com/study/event/api/event/service/EventUserService.java
+++ src/main/java/com/study/event/api/event/service/EventUserService.java
@@ -1,5 +1,6 @@
 package com.study.event.api.event.service;
 
+import com.study.event.api.event.dto.request.EventUserSaveDto;
 import com.study.event.api.event.entity.EmailVerification;
 import com.study.event.api.event.entity.EventUser;
 import com.study.event.api.event.repository.EmailVerificationRepository;
@@ -36,7 +37,7 @@ public class EventUserService {
         boolean exists = eventUserRepository.existsByEmail(email);
         log.info("Checking email {} is duplicate : {}", email, exists);
 
-        // 중복이 아니면 선제적으로 회원가입을 시킴(데이터베이스 처리, 이메일 보내는 것...)
+        // 일련의 후속 처리 (데이터베이스 처리, 이메일 보내는 것...)
         if (!exists) processSignUp(email);
 
         return exists;
@@ -52,6 +53,11 @@ public class EventUserService {
 
         EventUser savedUser = eventUserRepository.save(newEventUser);
 
+        generateAndSendCode(email, savedUser);
+
+    }
+
+    private void generateAndSendCode(String email, EventUser eventUser) {
         // 2. 이메일 인증 코드 발송
         String code = sendVerificationEmail(email);
 
@@ -59,50 +65,105 @@ public class EventUserService {
         EmailVerification verification = EmailVerification.builder()
                 .verificationCode(code) // 인증 코드
                 .expiryDate(LocalDateTime.now().plusMinutes(5)) // 만료 시간 (5분 뒤)
-                .eventUser(savedUser) // FK
+                .eventUser(eventUser) // FK
                 .build();
 
         emailVerificationRepository.save(verification);
-
     }
 
     // 이메일 인증 코드 보내기
     public String sendVerificationEmail(String email) {
 
+        // 검증 코드 생성하기
+        String code = generateVerificationCode();
+
         // 이메일을 전송할 객체 생성
         MimeMessage mimeMessage = mailSender.createMimeMessage();
 
         try {
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
 
-            // 검증 코드 생성하기
-            String code = generateVerificationCode();
-
-            // 누구한테 이메일을 보낼 것인지
-            mimeMessageHelper.setTo(email);
+            // 누구에게 이메일을 보낼 것인지
+            messageHelper.setTo(email);
             // 이메일 제목 설정
-            mimeMessageHelper.setSubject("[인증메일] 중앙정보스터디 가입 인증 메일");
+            messageHelper.setSubject("[인증메일] 중앙정보스터디 가입 인증 메일입니다.");
             // 이메일 내용 설정
-            mimeMessageHelper.setText("인증 코드: <b style=\"font-weight: 700; letter-spacing: 5px; font-size: 30px;\">" + code + "</b>",
-                    true);
+            messageHelper.setText(
+                    "인증 코드: <b style=\"font-weight: 700; letter-spacing: 5px; font-size: 30px;\">" + code + "</b>"
+                    , true
+            );
 
             // 전송자의 이메일 주소
-            mimeMessageHelper.setFrom(mailHost);
+            messageHelper.setFrom(mailHost);
 
             // 이메일 보내기
             mailSender.send(mimeMessage);
 
-            log.info("{}님에게 이메일 전송", email);
+            log.info("{} 님에게 이메일 전송!", email);
 
             return code;
 
         } catch (Exception e) {
-            throw new RuntimeException();
+            throw new RuntimeException(e);
         }
     }
 
-    // 검증 코드 생성 로직 1000~9999 사이의 숫자 4자리
+    // 검증 코드 생성 로직 1000~9999 사이의 4자리 숫자
     private String generateVerificationCode() {
         return String.valueOf((int) (Math.random() * 9000 + 1000));
+    }
+
+    // 인증코드 체크
+    public boolean isMatchCode(String email, String code) {
+
+        // 이메일을 통해 회원정보를 탐색
+        EventUser eventUser = eventUserRepository.findByEmail(email)
+                .orElse(null);
+
+        if (eventUser != null) {
+            // 인증코드가 있는지 탐색
+            EmailVerification ev = emailVerificationRepository.findByEventUser(eventUser).orElse(null);
+
+            // 인증코드가 있고 만료시간이 지나지 않았고 코드번호가 일치할 경우
+            if (
+                    ev != null
+                            && ev.getExpiryDate().isAfter(LocalDateTime.now())
+                            && code.equals(ev.getVerificationCode())
+            ) {
+                // 이메일 인증여부 true로 수정
+                eventUser.setEmailVerified(true);
+                eventUserRepository.save(eventUser); // UPDATE
+
+                // 인증코드 데이터베이스에서 삭제
+                emailVerificationRepository.delete(ev);
+                return true;
+            } else {  // 인증코드가 틀렸거나 만료된 경우
+                // 인증코드 재발송
+                // 원래 인증 코드 삭제
+                emailVerificationRepository.delete(ev);
+
+                // 새인증코드 발급 이메일 재전송
+                // 데이터베이스에 새 인증코드 저장
+                generateAndSendCode(email, eventUser);
+                return false;
+            }
+
+        }
+        return false;
+    }
+
+    // 회원가입 마무리
+    public void confirmSignUp(EventUserSaveDto dto) {
+
+        // 기존 회원 정보 조회
+        EventUser foundUser = eventUserRepository
+                .findByEmail(dto.getEmail())
+                .orElseThrow(
+                        () -> new RuntimeException("회원 정보가 존재하지 않습니다.")
+                );
+
+        // 데이터 반영(패스워드,가입시간)
+        foundUser.confirm(dto.getPassword());
+        eventUserRepository.save(foundUser);
     }
 }
