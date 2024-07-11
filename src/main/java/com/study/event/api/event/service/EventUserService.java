dc8d0b150933438942805169d57704d970cd52f7
oooowb <oooo940212@gmail.com>
2024-07-11 09:24:14 +0900

    인증 이메일 발송 데이터베이스 처리

--- src/main/java/com/study/event/api/event/service/EventUserService.java
+++ src/main/java/com/study/event/api/event/service/EventUserService.java
@@ -1,17 +1,19 @@
 package com.study.event.api.event.service;
 
+import com.study.event.api.event.entity.EmailVerification;
 import com.study.event.api.event.entity.EventUser;
+import com.study.event.api.event.repository.EmailVerificationRepository;
 import com.study.event.api.event.repository.EventUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.internet.MimeMessage;
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -23,6 +25,7 @@ public class EventUserService {
     private String mailHost;
 
     private final EventUserRepository eventUserRepository;
+    private final EmailVerificationRepository emailVerificationRepository;
 
     // 이메일 전송 객체
     private final JavaMailSender mailSender;
@@ -32,11 +35,39 @@ public class EventUserService {
 
         boolean exists = eventUserRepository.existsByEmail(email);
         log.info("Checking email {} is duplicate : {}", email, exists);
+
+        // 중복이 아니면 선제적으로 회원가입을 시킴(데이터베이스 처리, 이메일 보내는 것...)
+        if (!exists) processSignUp(email);
+
         return exists;
     }
 
+    public void processSignUp(String email) {
+
+        // 1. 임시 회원가입
+        EventUser newEventUser = EventUser
+                .builder()
+                .email(email)
+                .build();
+
+        EventUser savedUser = eventUserRepository.save(newEventUser);
+
+        // 2. 이메일 인증 코드 발송
+        String code = sendVerificationEmail(email);
+
+        // 3. 인증 코드 정보를 데이터베이스에 저장
+        EmailVerification verification = EmailVerification.builder()
+                .verificationCode(code) // 인증 코드
+                .expiryDate(LocalDateTime.now().plusMinutes(5)) // 만료 시간 (5분 뒤)
+                .eventUser(savedUser) // FK
+                .build();
+
+        emailVerificationRepository.save(verification);
+
+    }
+
     // 이메일 인증 코드 보내기
-    public void sendVerificationEmail(String email) {
+    public String sendVerificationEmail(String email) {
 
         // 이메일을 전송할 객체 생성
         MimeMessage mimeMessage = mailSender.createMimeMessage();
@@ -62,6 +93,9 @@ public class EventUserService {
             mailSender.send(mimeMessage);
 
             log.info("{}님에게 이메일 전송", email);
+
+            return code;
+
         } catch (Exception e) {
             throw new RuntimeException();
         }
